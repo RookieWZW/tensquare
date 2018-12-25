00c03288107b5c00f662b466c00058f45192209f
RookieWangZhiWei <RookieWangZhiWei@163.com>
2018-12-25 15:55:10 +0800

    rabbitMQ

---
+++ tensquare_sms/src/main/java/com/tensquare/sms/SmsApplication.java
@@ -0,0 +1,15 @@
+package com.tensquare.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by RookieWangZhiWei on 2018/12/25.
+ */
+@SpringBootApplication
+public class SmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmsApplication.class, args);
+    }
+}
